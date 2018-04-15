9309714fda024ff3807cdea6e0178a0ffe72a785
Jouni Roivas <jroivas@iki.fi>
2018-04-15 11:03:31 +0300

    Initialzie pseudo random generator as late as possible

--- arch/random.cpp
+++ arch/random.cpp
@@ -3,16 +3,18 @@
 
 Random::Random()
 {
-    init();
+    rand_seed = 0;
 }
 
 void Random::init()
 {
+    if (rand_seed != 0) return;
     rand_seed = Platform::timer()->getTicks();
 }
 
 unsigned int Random::getRandomInt()
 {
+    init();
     // Crappy and simple random number gen
     rand_seed = rand_seed * 1103515245 + 12345;
     return (unsigned int)(rand_seed / 65536) % 32768;
